3d764f1f643c9bc816beca57ce183c81a6e7e4bb
feng <lifeng@thinkive.com>
2018-10-18 09:57:56 +0800

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="hello">
+        <h1>{{ msg }}</h1>
+        <scroll :height="200">
+            fdsfasd
+        </scroll>
+    </div>
+</template>
+
+<script>
+import scroll from "@/components/scroll.vue";
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      msg: "Welcome to Your Vue.js App"
+    };
+  },
+  components: {
+    scroll
+  }
+};
+</script>
---
+++ src/components/scroll.vue
@@ -0,0 +1,22 @@
+<template>
+    <div ref="wrapper">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+import BScroll from "better-scroll";
+export default {
+  props: {
+    height: {
+      type: Number
+    }
+  },
+  created() {
+    this.$nextTick(() => {
+      this.$refs.wrapper.style.height = this.height + "px";
+    });
+  }
+};
+</script>
+
